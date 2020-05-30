3b8268d78ab64801257454a8c707610c98c525f6
Ryota Goto <ryotagoto8@yahoo.co,jp>
2020-05-30 15:19:04 +0900

    modify

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -3,7 +3,7 @@
 import unittest, rostest
 import rosnode, rospy
 import time
-from pimouse_ros.msg import LightSensorValues
+from test_rosbook.msg import LightSensorValues
         
 class LightsensorTest(unittest.TestCase):
     def setUp(self):
