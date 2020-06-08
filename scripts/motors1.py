154abbc1f2969d2d8fe9995aae3ef4f5a9b5d5aa
Ryota Goto <ryotagoto8@yahoo.co,jp>
2020-06-08 20:26:19 +0900

    change

--- scripts/motors1.py
+++ scripts/motors1.py
@@ -6,24 +6,23 @@ from geometry_msgs.msg import Twist
 class Motor():
     def __init__(self):
         self.set = Set()
-        if not self.set.power(True): sys.exist(1)
+        if not self.power(True): sys.exist(1)
 
-        rospy.on_shutdown(self.set.power)
+        rospy.on_shutdown(self.power)
         self.sub_raw = rospy.Subscriber('motor_raw', MotorFreqs, self.callback_raw_freq)
         self.sub_cmd_vel = rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
         self.last_time = rospy.Time.now()
  
-    def callback_raw_freq(self,message):
-        self.set.raw_freq(message.left_hz,message.right_hz)
+    def callback_raw_freq(self, message):
+        self.raw_freq(message.left_hz,message.right_hz)
     
     def callback_cmd_vel(self, message):
         forward_hz = 80000.0*message.linear.x/(9*math.pi)
         rot_hz = 400.0*message.angular.z/math.pi
         self.set.raw_freq(forward_hz-rot_hz, forward_hz+rot_hz)
-      #  self.using_cmd_vel = True
+        self.using_cmd_vel = True
         self.last_time = rospy.Time.now()
 
-class Set:
     def power(self,onoff = False):
         dev = '/dev/rtmotoren0'
         try:
@@ -54,7 +53,7 @@ if __name__ == '__main__':
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        if rospy.Time.now().to_sec() - m.last_time.to_sec() >= 1.0:
-            m.set.raw_freq(0,0)
+        if m.using_cmd_vel and rospy.Time.now().to_sec() - m.last_time.to_sec() >= 1.0:
+            m.raw_freq(0,0)
             m.using_cmd_vel = False
         rate.sleep()
